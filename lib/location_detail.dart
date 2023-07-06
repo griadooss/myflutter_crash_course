31b35ae45c496a0a084dc267c47cbfd84100662e
griadooss <github@zimpics.com>
2023-07-06 14:04:22 +0800

    Moved code to Location Details

---
+++ lib/location_detail.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class LocationDetail extends StatelessWidget {
+  const LocationDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World!'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _section('one', Colors.red),
+            _section('two', Colors.yellow),
+            _section('three', Colors.blue),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget _section(String title, Color color) {
+  return Container(
+    color: color,
+    child: Text(title),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myflutter_crash_course/location_detail.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello World!'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.red,
-              child: const Text('one'),
-            ),
-            Container(
-              color: Colors.yellow,
-              child: const Text('two'),
-            ),
-            Container(
-              color: Colors.blue,
-              child: const Text('three'),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
+  runApp(const LocationDetail());
 }
