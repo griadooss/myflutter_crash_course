d40902f460f8f76182c509c480bb5045429ba902
griadooss <github@zimpics.com>
2023-07-06 13:26:12 +0800

    Add three containers

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,24 @@ void main() {
         appBar: AppBar(
           title: const Text('Hello World!'),
         ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              child: const Text('one'),
+            ),
+            Container(
+              color: Colors.yellow,
+              child: const Text('two'),
+            ),
+            Container(
+              color: Colors.blue,
+              child: const Text('three'),
+            ),
+          ],
+        ),
       ),
     ),
   );
